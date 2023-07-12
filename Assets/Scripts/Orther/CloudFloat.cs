2d8fa31ddb372522cceddb782b25b0f49413839b
hnmluan <121085924+hnmluan@users.noreply.github.com>
2023-07-13 03:05:28 +0700

    Weather cloud

--- Assets/Scripts/Orther/CloudFloat.cs
+++ Assets/Scripts/Orther/CloudFloat.cs
@@ -1,28 +1,75 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CloudFloat : MonoBehaviour
 {
     public Transform startPoint;
-
     public Transform endPoint;
-
     public float speed;
+    public float amplitude;
+    public float frequency;
+    public AnimationCurve fadeCurve;
+    public float waitTimeToStart = 2f; // Thời gian đợi trước khi bắt đầu di chuyển (giây)
+
+    private float timeOffset;
+    private float distanceTraveled;
+    private bool isWaitingTimeToStart;
+
+    private Renderer cloud;
 
     private void Start()
     {
         transform.parent.position = startPoint.position;
+        distanceTraveled = 0f;
+        isWaitingTimeToStart = true;
+        StartCoroutine(WaitAndStartMoving());
+        cloud = transform.parent.GetComponentInChildren<Renderer>();
     }
 
     private void Update()
     {
+        if (isWaitingTimeToStart) return;
+
+        Move();
+        ApplyOscillation();
+        UpdateFade();
+        CheckArrival();
+    }
+
+    private void Move()
+    {
         Vector3 direction = (endPoint.position - startPoint.position).normalized;
-
         transform.parent.position += direction * speed * Time.deltaTime;
+        distanceTraveled += speed * Time.deltaTime;
+    }
 
-        if (Vector3.Distance(transform.parent.position, endPoint.position) < 0.1f)
+    private void ApplyOscillation()
+    {
+        timeOffset += Time.deltaTime * frequency;
+        Vector3 oscillation = Mathf.Sin(timeOffset) * amplitude * transform.right;
+        transform.parent.position += oscillation;
+    }
+
+    private void UpdateFade()
+    {
+        float t = distanceTraveled / Vector3.Distance(startPoint.position, endPoint.position);
+        float fadeValue = fadeCurve.Evaluate(t);
+        Color currentColor = cloud.material.color;
+        Color fadedColor = new Color(currentColor.r, currentColor.g, currentColor.b, fadeValue);
+        cloud.material.color = fadedColor;
+    }
+
+    private void CheckArrival()
+    {
+        if (distanceTraveled >= Vector3.Distance(startPoint.position, endPoint.position))
         {
             transform.parent.position = startPoint.position;
+            distanceTraveled = 0f;
         }
     }
 
+    private System.Collections.IEnumerator WaitAndStartMoving()
+    {
+        yield return new WaitForSeconds(waitTimeToStart);
+        isWaitingTimeToStart = false;
+    }
 }
