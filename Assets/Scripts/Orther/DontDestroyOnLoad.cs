590d6f2716cfced1dc471e979c22416d071f36d7
hnmluan <121085924+hnmluan@users.noreply.github.com>
2023-07-19 11:55:44 +0700

    LoadScene

---
+++ Assets/Scripts/Orther/DontDestroyOnLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    private static DontDestroyOnLoad instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
