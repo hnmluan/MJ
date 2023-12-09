ae570b0bda824f9fd55662816020b483bcef054f
hnmluan <121085924+hnmluan@users.noreply.github.com>
2023-12-09 11:28:03 +0700

    Switch Weapon

--- Assets/Scripts/Actor/Player/PlayerActtack.cs
+++ Assets/Scripts/Actor/Player/PlayerActtack.cs
@@ -1,5 +1,12 @@
 public class PlayerActtack : ObjAttackByMouse
 {
+    private void Update()
+    {
+        if (InputManager.Instance.FocusFirstWeapon()) Armory.Instance.FocusItem(1);
+        if (InputManager.Instance.FocusSecondWeapon()) Armory.Instance.FocusItem(2);
+        if (InputManager.Instance.FocusThirdWeapon()) Armory.Instance.FocusItem(3);
+    }
+
     protected override void Attacking()
     {
         if (Armory.Instance.GetFocusEquippedWeapon() == null) return;
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class InputManager : Singleton<InputManager>
 {
     public bool StartInteract() => Input.GetKeyDown(KeyCode.I);
+
     public bool ContinueInteract() => Input.GetKeyDown(KeyCode.Space);
 
     private bool submitPressed = false;
@@ -21,11 +22,24 @@ public class InputManager : Singleton<InputManager>
     public void RegisterSubmitPressed() => submitPressed = true;
 
     public virtual bool OpenInventory() => false;
+
     public virtual bool OpenSetting() => false;
+
     public virtual bool OpenGuide() => false;
+
     public virtual bool OpenPause() => false;
+
     public virtual bool OpenArmory() => false;
+
     public virtual bool OpenDictionary() => false;
+
     public virtual bool OpenShop() => false;
+
     public virtual bool Close() => false;
+
+    public virtual bool FocusFirstWeapon() => Input.GetKeyDown(KeyCode.Alpha1);
+
+    public virtual bool FocusSecondWeapon() => Input.GetKeyDown(KeyCode.Alpha2);
+
+    public virtual bool FocusThirdWeapon() => Input.GetKeyDown(KeyCode.Alpha3);
 }
